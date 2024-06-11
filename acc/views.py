921306e284aedcd6bb6ebc3eabd49d2370a607d5
logesh raja <103126824+logeshr290@users.noreply.github.com>
2024-06-11 18:47:36 +0530

    Update views.py

--- acc/views.py
+++ acc/views.py
@@ -1,22 +1,21 @@
-from django.http import HttpResponse
 import logging
-from rest_framework.exceptions import NotFound
-from django.template.response import TemplateResponse
-from django.contrib.auth import logout
-from rest_framework import generics, permissions, status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.contrib.auth import get_user_model, authenticate, login
-from .models import FriendRequest
-from .serializers import UserSerializer, FriendRequestSerializer
-from django.db.models import Q
-from rest_framework.pagination import PageNumberPagination
-from django.utils import timezone
-from django.shortcuts import redirect, render
+from django.contrib.auth import logout, authenticate, login, get_user_model
+from django.shortcuts import render, redirect
 from django.urls import reverse
-from rest_framework.authtoken.models import Token
+from django.utils import timezone
 from datetime import timedelta
-from .serializers import UserSignupSerializer, UserLoginSerializer
+from django.db.models import Q
+from django.core.paginator import Paginator
+
+from rest_framework import generics, permissions, status
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from .models import FriendRequest
+from .forms import UserSignupForm, UserLoginForm
+from .serializers import UserSerializer
 
 User = get_user_model()
 
@@ -25,42 +24,39 @@ def home(request):
 
 class UserSignupView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
-    serializer_class = UserSignupSerializer
 
     def get(self, request, *args, **kwargs):
-        return Response({
-            "message": "Provide email, username, and password to sign up.",
-            "fields": ["email", "username", "password"]
-        })
+        form = UserSignupForm()
+        return render(request, 'signup.html', {'form': form})
 
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
+        form = UserSignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
             login(request, user)
             return redirect(reverse('home'))
-            return Response({"message": "Signup successful"}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return render(request, 'signup.html', {'form': form})
 
 class UserLoginView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
-    serializer_class = UserLoginSerializer
 
     def get(self, request, *args, **kwargs):
-        return Response({
-            "message": "Provide email and password to log in.",
-            "fields": ["email", "password"]
-        })
+        form = UserLoginForm()
+        return render(request, 'login.html', {'form': form})
 
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        login(request, user)
-        return redirect(reverse('list-friends'))
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse('list-friends'))
+        return render(request, 'login.html', {'form': form})
 
 class UserSearchView(generics.ListAPIView):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     serializer_class = UserSerializer
     pagination_class = PageNumberPagination
 
@@ -77,9 +73,8 @@ class UserSearchView(generics.ListAPIView):
             Q(first_name__icontains=query)
         )
 
-
 class ListFriendsView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
         user = request.user
@@ -92,29 +87,41 @@ class ListFriendsView(APIView):
         potential_friends = User.objects.exclude(
             Q(id=user.id) | Q(sent_requests__receiver=user, sent_requests__status='accepted') |
             Q(received_requests__sender=user, received_requests__status='accepted')
-        )
+        ).order_by('username')
         if search_query:
             potential_friends = potential_friends.filter(
                 Q(username__icontains=search_query) | Q(email__icontains=search_query)
             )
 
+        paginator = Paginator(potential_friends, 10)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
         pending_requests = FriendRequest.objects.filter(receiver=user, status='pending')
+        sent_requests = FriendRequest.objects.filter(sender=user, status='pending')
+
         context = {
             'friends': friends,
-            'potential_friends': potential_friends,
+            'page_obj': page_obj,
             'pending_requests': pending_requests,
+            'sent_requests': sent_requests,
             'search_query': search_query,
         }
-        return TemplateResponse(request, 'list_friends.html', context)
+        return render(request, 'list_friends.html', context)
 
-
+    def update_potential_friends(self, user):
+        potential_friends = User.objects.exclude(
+            Q(id=user.id) | Q(sent_requests__receiver=user, sent_requests__status='accepted') |
+            Q(received_requests__sender=user, received_requests__status='accepted')
+        )
+        return potential_friends
 
 def logout_view(request):
     logout(request)
     return redirect(reverse('home'))
 
 class SendFriendRequestView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         sender = request.user
@@ -130,12 +137,13 @@ class SendFriendRequestView(APIView):
         if FriendRequest.objects.filter(sender=sender, receiver=receiver).exists():
             return Response({"error": "Friend request already sent."}, status=status.HTTP_400_BAD_REQUEST)
 
+
         friend_request = FriendRequest(sender=sender, receiver=receiver)
         friend_request.save()
         return Response({"message": "Friend request sent."}, status=status.HTTP_201_CREATED)
 
 class AcceptFriendRequestView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         sender_id = request.data.get('sender_id')
@@ -143,16 +151,26 @@ class AcceptFriendRequestView(APIView):
         if not sender_id:
             return Response({"error": "Sender ID is required."}, status=status.HTTP_400_BAD_REQUEST)
 
-        logging.info(f"Received sender_id: {sender_id}, Current user: {request.user}")
-
         try:
             friend_request = FriendRequest.objects.get(sender_id=sender_id, receiver=request.user, status='pending')
-        except FriendRequest.DoesNotExist as e:
-            logging.error(f"Friend request does not exist: {e}")
+        except FriendRequest.DoesNotExist:
             return Response({"error": "Friend request does not exist."}, status=status.HTTP_404_NOT_FOUND)
         friend_request.status = 'accepted'
         friend_request.save()
         return Response({"message": "Friend request accepted."}, status=status.HTTP_200_OK)
 
 class RejectFriendRequestView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        sender_id = request.data.get('sender_id')
+
+        if not sender_id:
+            return Response({"error": "Sender ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            friend_request = FriendRequest.objects.get(sender_id=sender_id, receiver=request.user, status='pending')
+        except FriendRequest.DoesNotExist:
+            return Response({"error": "Friend request does not exist."}, status=status.HTTP_404_NOT_FOUND)
+        friend_request.delete()
+        return Response({"message": "Friend request rejected."}, status=status.HTTP_200_OK)
