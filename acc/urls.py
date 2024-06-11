848c85a1f2ade42fb9e4edd9c28d49c86d328821
logesh raja <103126824+logeshr290@users.noreply.github.com>
2024-06-11 18:48:46 +0530

    Update urls.py

--- acc/urls.py
+++ acc/urls.py
@@ -1,22 +1,14 @@
 from django.urls import path
-from .views import (
-    home,
-    UserSignupView,
-    UserLoginView,
-    ListFriendsView,
-    SendFriendRequestView,
-    AcceptFriendRequestView,
-    RejectFriendRequestView,
-    logout_view,
-)
+from .views import home, UserSignupView, UserLoginView, UserSearchView, ListFriendsView, logout_view, SendFriendRequestView, AcceptFriendRequestView, RejectFriendRequestView
 
 urlpatterns = [
     path('', home, name='home'),
     path('signup/', UserSignupView.as_view(), name='signup-page'),
     path('login/', UserLoginView.as_view(), name='login-page'),
     path('logout/', logout_view, name='logout'),
-    path('friends/', ListFriendsView.as_view(), name='list-friends'),
-    path('send-friend-request/', SendFriendRequestView.as_view(), name='send-friend-request'),
-    path('accept-friend-request/', AcceptFriendRequestView.as_view(), name='accept-friend-request'),
-    path('reject-friend-request/', RejectFriendRequestView.as_view(), name='reject-friend-request'),
+    path('search', UserSearchView.as_view(), name='search-users'),
+    path('friends', ListFriendsView.as_view(), name='list-friends'),
+    path('send-friend-request', SendFriendRequestView.as_view(), name='send-friend-request'),
+    path('accept-friend-request', AcceptFriendRequestView.as_view(), name='accept-friend-request'),
+    path('reject-friend-request', RejectFriendRequestView.as_view(), name='reject-friend-request'),
 ]
